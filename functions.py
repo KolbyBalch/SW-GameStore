d7b167e1730dab5517216a6c68c1ad9130a55783
Sean-Luc <103013595+Sean-Luc@users.noreply.github.com>
2022-04-19 19:40:16 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -61,9 +61,10 @@ def sq(num):
 ## greets them by their entire name
 ## names should be strings
 def greetUser(first, middle, last):
-    print("Hello!")
-    print("Welcome to the program", first, middle, last)
-    print("Glad to have you!")
+     if type(first) == str and type(middle) == str and type(last) == str:
+        print("Hello!")
+        print("Welcome to the program", first, middle, last)
+        print("Glad to have you!")
 
 ## takes in a Python list
 ## attempts to display the item at the index provided
